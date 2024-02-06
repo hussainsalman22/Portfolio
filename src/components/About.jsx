12642c4b39c38b3e0d8747b66e6cfea2e7dc5af1
hussainsalman22 <hussainqureshi22jan@gmail.com>
2024-02-06 14:32:04 +0500

    first commit

---
+++ src/components/About.jsx
@@ -0,0 +1,28 @@
+import aboutSvg from '../assets/about.svg';
+import SectionTitle from './SectionTitle';
+const About = () => {
+  return (
+    <section className='bg-white py-20' id='about'>
+      <div className='align-element grid md:grid-cols-2 items-center gap-16'>
+        <img src={aboutSvg} className='w-full h-64' />
+        <article>
+          <SectionTitle text='About Me' />
+          <p className='text-slate-600 mt-8 leading-loose'>
+            🎓 Currently pursuing a degree in Mechatronics Engineering at Mehran University with a track record of excellence, having completed Intermediate with an A1 grade.<br/>
+
+            💻 Proficient in MERN Stack, Next.js, Firebase, and skilled in leveraging GitHub for version control. Actively diving into the world of React Native with Firebase, driven by a passion for honing versatile development skills.<br/>
+
+            🌟 Eager to contribute and learn, I'm currently undertaking online projects pro bono to craft a robust portfolio. My dedication and hard work fuel my growth as I aim to solidify my expertise.<br/>
+
+            🤝 A team player at heart, I excel in collaborative environments, leveraging strong communication skills to foster productive teamwork and achieve shared goals.
+
+            🔍 Open to opportunities that allow me to apply and expand my skill set in a dynamic setting. Let's connect and explore how we can collaborate!
+
+
+          </p>
+        </article>
+      </div>
+    </section>
+  );
+};
+export default About;
---
+++ src/data.jsx
@@ -0,0 +1,65 @@
+import { nanoid } from 'nanoid';
+import { FaHtml5, FaJs, FaReact } from 'react-icons/fa';
+
+export const links = [
+  { id: nanoid(), href: '#home', text: 'home' },
+  { id: nanoid(), href: '#skills', text: 'skills' },
+  { id: nanoid(), href: '#about', text: 'about' },
+  { id: nanoid(), href: '#projects', text: 'projects' },
+];
+
+export const skills = [
+  {
+    id: nanoid(),
+    title: 'HTML&CSS',
+    icon: <FaHtml5 className='h-16 w-16 text-emerald-500' />,
+    text: 'Highly skilled in HTML & CSS, adeptly crafting visually appealing and responsive websites for optimal user experiences.',
+  },
+  {
+    id: nanoid(),
+    title: 'Javascript',
+    icon: <FaJs className='h-16 w-16 text-emerald-500' />,
+    text: 'Expertise in JavaScript, building interactive and dynamic web applications with a focus on seamless user interactions and functionality',
+  },
+  {
+    id: nanoid(),
+    title: 'React',
+    icon: <FaReact className='h-16 w-16 text-emerald-500' />,
+    text: 'Advanced proficiency in React, developing efficient and interactive front-end applications with a strong emphasis on component-based architecture.',
+  },
+];
+
+export const projects = [
+  {
+    id: nanoid(),
+    img: 'https://images.pexels.com/photos/905163/pexels-photo-905163.jpeg?auto=compress&cs=tinysrgb&w=600',
+    url: 'https://agency-kappa-ashy.vercel.app/',
+    github: 'https://github.com/hussainsalman22/Agency',
+    title: 'Agency',
+    text: 'Agency website with Nextjs and tailwind css',
+  },
+  {
+    id: nanoid(),
+    img: 'https://images.pexels.com/photos/1552252/pexels-photo-1552252.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://hussainsalman22.github.io/gym/',
+    github: 'https://github.com/hussainsalman22/gym/tree/main',
+    title: 'GYM WEBSITE',
+    text: 'Build a gym website with react and bootstrap.',
+  },
+  {
+    id: nanoid(),
+    img: 'https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://react-quiz-app-beta-ten.vercel.app/',
+    github: 'https://github.com/hussainsalman22/React-quiz-app-4/tree/main',
+    title: 'React Quiz App',
+    text: 'Quiz app with react and tailwind css',
+  },
+  {
+    id: nanoid(),
+    img: 'https://images.pexels.com/photos/301920/pexels-photo-301920.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
+    url: 'https://react-quiz-app-beta-ten.vercel.app/',
+    github: 'https://github.com/hussainsalman22/React-quiz-app-4/tree/main',
+    title: 'React Quiz App',
+    text: 'Quiz app with react and tailwind css',
+  },
+];
